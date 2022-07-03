60a598fe346390019f7b4283a33fdb71fe6a437e
pacry241 <phanduyman241@gmail.com>
2022-07-03 20:57:35 +0700

    upload

--- src/ss10_DSA_Danhsach/Bt/LinkedList/TestLinkedList.java
+++ src/ss10_DSA_Danhsach/Bt/LinkedList/TestLinkedList.java
@@ -2,30 +2,30 @@ package ss10_DSA_Danhsach.Bt.LinkedList;
 
 public class TestLinkedList {
     public static void main(String[] args) {
-        MyLinkedList<Integer> ml = new MyLinkedList<>();
-        ml.addFirst(1);
-        ml.addFirst(2);
-        ml.addFirst(3);
-        ml.addFirst(4);
-        ml.printList();
+        MyLinkedList<Integer> array = new MyLinkedList<>();
+        array.addFirst(1);
+        array.addFirst(2);
+        array.addFirst(3);
+        array.addFirst(4);
+        array.printList();
 
         System.out.println();
 
-        ml.addLast(7);
-        ml.add(2,2);
-        ml.printList();
+        array.addLast(7);
+        array.add(2,2);
+        array.printList();
 
         System.out.println();
-        ml.remove(4);
-        ml.printList();
+        array.remove(4);
+        array.printList();
 
         System.out.println();
-        System.out.print(ml.getFirst());
+        System.out.print(array.getFirst());
 
         System.out.println();
-        ml.clear();
-        ml.addFirst(0);
-        //  System.out.println(ml.size());
-        ml.printList();
+        array.clear();
+        array.addFirst(0);
+        //  System.out.println(array.size());
+        array.printList();
     }
 }
